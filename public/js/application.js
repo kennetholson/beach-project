c7de66f211672ff56abdf3befaf6a4d5c7034a00
Kenneth Olson <kenneth.ko.olson@gmail.com>
2016-07-26 10:59:17 -0700

    change table at index

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,21 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+	getSurfReport();
 });
+	
+function getSurfReport() {
+	$('#beach-info-form').on('submit', function(event){
+		event.preventDefault();
+		var formData = $('#beach').serialize()
+		console.log(formData);
+
+	$.ajax({
+	url: '/',
+	method: 'POST',
+	data: formData
+	})		
+	.done(function(serverData){
+	$('#surfcam').append(serverData);		
+	})
+  
+  });
+}
